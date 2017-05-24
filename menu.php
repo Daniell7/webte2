5c2c2a1ab0c82bf2a913b372a2de33654a2ddfa2
lukaskor <korciciak.lukas@gmail.com>
2017-05-24 17:11:16 +0200

    Add files via upload

--- menu.php
+++ menu.php
@@ -20,7 +20,7 @@ body {
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">NavBar</a>
+            <a class="navbar-brand" href="index.php">Hlavná stránka</a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
