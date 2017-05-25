68496602a691d1da7683d015603116bfc54c2f9b
lukaskor <korciciak.lukas@gmail.com>
2017-05-25 12:09:14 +0200

    media hotovo

---
+++ media.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Média</title>
+    <link href="css/menu.css" rel="stylesheet">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="image/favicon.png" type="image/png" sizes="16x16">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<?php
+include 'menu.php';
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Vďaka biomechatronikom z STU sa už akupunktúrne body neskryjú</h2>
+        </div>
+        <div class="col-md-8">
+            <p>Dátum zverejnenia: 11.3.2016</p>
+            <p>Zdroj: Science.sk - Lenka Smataníková</p><br>
+            <p>Čoraz viac ľudí vyhľadáva alternatívne liečebné postupy, medzi ktoré
+                patrí aj akupunktúra. Veľakrát je ale zložité nájsť dôveryhodného
+                odborníka, ktorý telu skutočne pomôže. Vedci v z Ústavu
+                automobilovej mechatroniky FEI STU STU zostrojili sondu na
+                snímanie povrchu pokožky, ktorá by tento problém mohla vyriešiť...</p>
+            <p>Viac nájdete tu: <a href="media/science20162903.pdf" target="_blank">science20162903.pdf</a></p>
+        </div>
+        <div class="col-md-4">
+            <img class="img-responsive" src="media/foto_k_clanku/science20162903.png" alt="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Poodkryl tajomstvo</h2>
+        </div>
+        <div class="col-md-8">
+            <p>Dátum zverejnenia: 4.6.2015</p>
+            <p>Zdroj: Šarm</p><br>
+            <p>Dnes už síce ani v našich
+                končinách nedávame
+                znak rovnosti medzi
+                akupunktúru a šarlatánstvo,
+                ale aj tak máme radšej
+                také spôsoby liečenia, aké
+                vieme presne objasniť. Biomechatronikovi
+                z Ústavu automobilovej
+                mechatroniky,
+                Fakulty elektrotechniky a informatiky
+                STU v Bratislave
+                sa spolu s niekoľkými spolupracovníkovi
+                podarilo zostrojiť
+                prístroj, ktorý azda zmenší
+                rady skeptikov medzi vedcami
+                aj lekármi...</p>
+            <p>Viac nájdete tu: <a href="media/sarm201546.pdf" target="_blank">sarm201546.pdf</a></p>
+        </div>
+        <div class="col-md-4">
+            <img class="img-responsive" src="media/foto_k_clanku/sarm201546.jpg" alt="">
+        </div>
+    </div>
+
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Linky -->
+    <div class="well">
+        <div class="row">
+            <div class="col-md-8">
+                <a href="http://is.stuba.sk/">AIS STU</a><br>
+                <a href="http://aladin.elf.stuba.sk/rozvrh/">Rozvrh hodín FEI</a><br>
+                <a href="http://elearn.elf.stuba.sk/moodle/">Moodle FEI</a><br>
+                <a href="http://www.sski.sk/webstranka/">SSKI</a><br>
+                <a href="https://www.jedalen.stuba.sk/WebKredit/">Jedáleň STU</a><br>
+                <a href="https://webmail.stuba.sk/">Webmail STU</a><br>
+                <a href="https://kis.cvt.stuba.sk/i3/epcareports/epcarep.csp?ictx=stu&language=1">Evidencia publikácií STU</a><br>
+                <a href="http://okocasopis.sk/">Časopis OKO</a><br>
+                <a href="https://www.facebook.com/UAMTFEISTU">Facebook ústavu</a><br>
+                <a href="https://www.youtube.com/channel/UCo3WP2kC0AVpQMIiJR79TdA">Youtube ústavu</a><br>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; DIvan LKorciciak DPetrik PFuchs PChynoransky</p>
+            </div>
+        </div>
+    </footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Script to Activate the Carousel -->
+<script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+</script>
+
+</body>
+
+</html>
